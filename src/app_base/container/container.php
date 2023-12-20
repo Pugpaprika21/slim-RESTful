f39c7f039b45aeb6c8c8dbae4f70f3b70e1bec50
Pugpaprika21 <pugpaprika21@gmail.com>
2023-12-20 05:51:28 -0800

    edit R

--- src/app_base/container/container.php
+++ src/app_base/container/container.php
@@ -3,10 +3,11 @@
 $container->set('db', function () {
     global $env;
     if (!empty($env['DB_USERNAME'])) {
-        R::setup($env['DB_CONNECT_DNS'], $env['DB_USERNAME'], '');
-        R::debug(false);
-        R::freeze(true);
-        return new R();
+        $rb = new R();
+        $rb->setup($env['DB_CONNECT_DNS'], $env['DB_USERNAME'], '');
+        $rb->debug(false);
+        $rb->freeze(true);
+        return $rb;
     }
     throw new Exception('db name not define ..');
 });
